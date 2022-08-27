5eaa250190bc12570cfafe960ee3fb8b56d3c772
Wisterleys <wister.sousa2@gmail.com>
2022-08-27 10:25:14 -0300

    Initial commit

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network("https://developers.giphy.com/branch/master/static/header-logo-0fec0225d189bc0eae27dac3e3770582.gif"),
+        centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                alignLabelWithHint: true,
+                label: Text("Digite sua pesquisa")
+                ),
+            )
+          ],
+        ),
+    );
+  }
+}
